23cc33825ad07112985bee4bf1013cbba98efe6e
BaroSquixZ <84010277+BaroSquixZ@users.noreply.github.com>
2021-06-23 11:29:39 +0200

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,15 @@
+ <?php 
+
+$nombre = $_POST['nombre'];
+
+$apellidos = $_POST['apellidos'];
+
+$valoracion = $_POST['valoracion']
+
+echo "<br/> &nbsp; Nombre: ". $nombre.;
+
+echo "<br/> &nbsp; Apellidos: ". $apellidos.;
+
+echo "<br/> &nbsp; Valoracion: ". $valoracion.<br/>;
+
+?>
